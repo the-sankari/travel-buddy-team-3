95bc3b437e74574d58b498b1d591e2e2aaba4c08
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-05-30 01:11:53 +0300

    Weather forecast and searchbox updated

---
+++ web/frontend/src/components/ForcastCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ForcastCard = (weather) => {
+  const date = new Date(weather.dt_txt);
+  const dayname = date.toLocaleDateString("en-US", { weekday: "short" });
+  return (
+    <div className="card forecast-card text-center">
+      <div className="card-body">
+        <h5 className="card-title">{dayname}</h5>
+        <img
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt={`${weather.weather[0].description} icon`}
+        />
+        <p className="card-text">{Math.round(weather.main.temp)} °C</p>
+      </div>
+    </div>
+  );
+};
+
+export default ForcastCard;
+
+
+// , {weather.weather[0].description}
