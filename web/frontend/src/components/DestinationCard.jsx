2f3aea4a912375d041d0f0c3a81a6b610d07d6ef
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-29 13:04:40 +0300

    Update DestinationCard

--- web/frontend/src/components/DestinationCard.jsx
+++ web/frontend/src/components/DestinationCard.jsx
@@ -1,20 +1,12 @@
-import { getCountryInfo } from "../services/countriesService";
-import Map from "../components/Map";
+import React from "react";
+import CountryData from "./CountryData";
+import MapComponent from "./MapComponent";
 
-const DestinationCard = () => {
+const DestinationCard = ({ countryName, lat, lon, mapRef }) => {
   return (
     <>
-      <div>DestinationCard</div>
-      {countryInfo && (
-        <div>
-          <h2>{countryInfo.name.common}</h2>
-          <p>Capital: {countryInfo.capital[0]}</p>
-          <p>Population: {countryInfo.population}</p>
-        </div>
-      )}
-      {coordinates.longitude !== 0 && coordinates.latitude !== 0 && (
-        <Map coordinates={coordinates} />
-      )}
+      {countryName && <CountryData countryName={countryName} />}
+      <MapComponent lat={lat} lon={lon} mapRef={mapRef} />
     </>
   );
 };
