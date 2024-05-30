d862baf1d13ab1d45c65d8e54574efe0670d0147
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-30 12:48:59 +0300

    Add getting user's location coordinates, pass to TripPlanner

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -1,5 +1,4 @@
-// src/App.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import HomePage from "./pages/HomePage";
@@ -8,6 +7,38 @@ import ContactPage from "./pages/ContactPage";
 import LoginPage from "./pages/LoginPage";
 
 const App = () => {
+  const [initialCoords, setInitialCoords] = useState({
+    // Fallback coordinates (Kouvola) if geolocation fails:
+    lat: 60.867872,
+    lon: 26.704129,
+  });
+  const [error, setError] = useState(null);
+
+  // Get coordinates for user's current location:
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setInitialCoords({ lat: latitude, lon: longitude });
+        },
+        (err) => {
+          setError("Failed to retrieve user location");
+          console.error(err);
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  // Log geolocation error
+  useEffect(() => {
+    if (error) {
+      console.error("Geolocation error:", error);
+    }
+  }, [error]);
+
   const routes = [
     {
       path: "/",
@@ -19,7 +50,7 @@ const App = () => {
         },
         {
           path: "/planner",
-          element: <TripPlanner />,
+          element: <TripPlanner initialCoords={initialCoords} />,
         },
         { path: "/contact", element: <ContactPage /> },
         {
