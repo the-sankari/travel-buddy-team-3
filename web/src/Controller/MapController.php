6b08450a75cf785cc0c0af87e1ec4fc5626c07dd
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-31 10:40:27 +0300

    Add getting displayedCityName from Geocoding API response

--- web/src/Controller/MapController.php
+++ web/src/Controller/MapController.php
@@ -41,7 +41,7 @@ class MapController extends AbstractController
         $lat = $data['features'][0]['center'][1];
         $lon = $data['features'][0]['center'][0];
 
-        // Find country name from response:
+        // Find country name from response (to be used for REST countries API):
         $countryName = null;
         foreach ($data['features'][0]['context'] as $item) {
             if (strpos($item['id'], 'country') === 0) {
@@ -49,8 +49,11 @@ class MapController extends AbstractController
                 break;
             }
         }
-        
-        // Fetch weather data from OpenWeatherMap API
+
+        // Find city name from response:
+        $displayedCityName = $data['features'][0]['text'];
+
+        // Fetch weather data from OpenWeatherMap API:
         $weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={$lat}&lon={$lon}&appid={$this->WEATHER_KEY}&units=metric";
         $weatherResponse = $this->httpClient->request('GET', $weatherUrl);
         $weatherData = $weatherResponse->toArray();
@@ -58,7 +61,8 @@ class MapController extends AbstractController
         return new JsonResponse([
             'coordinates' => ['lng' => $coordinates[0], 'lat' => $coordinates[1]],
             'weather' => $weatherData,
-            'countryName' => $countryName
+            'countryName' => $countryName,
+            'displayedCityName' => $displayedCityName
         ]);
     }
 }
