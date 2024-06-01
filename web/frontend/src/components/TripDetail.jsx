421ea522f5a883a1a106f95199e1805648442c8a
Sulaimon Ekundayo <topdandydot@gmail.com>
2024-06-01 21:25:37 +0300

    Add component to display traveller trip information

---
+++ web/frontend/src/components/TripDetail.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const TripDetail = () => {
+  const { id } = useParams();
+  const [trip, setTrip] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8007/api/trips/${id}`)
+      .then((response) => response.json())
+      .then((data) => setTrip(data))
+      .catch((error) => console.error("Error fetching trip:", error));
+  }, [id]);
+
+  if (!trip) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Trip Overview</h2>
+        <p>Destinations: {trip.destination}</p>
+        <p>
+          Travel Dates: {trip.checkIn} - {trip.checkOut}
+        </p>
+      </div>
+      <div>
+        <h2>Traveler Information</h2>
+        <div>
+          <div>
+            <p>Name: {trip.name}</p>
+            <p>Email: {trip.email}</p>
+            <p>Mobile: {trip.mobile}</p>
+            {/* <p>Longitude: {trip.longitude}</p> */}
+            {/* <p>Latitude: {trip.latitude}</p> */}
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default TripDetail;
