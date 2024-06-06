930b1d3dd57c84fd6b12d0c5350d1bfc24997f7e
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-06-06 16:25:49 +0300

    Separate plannedTrips and pastTrips by comparing checkOut to current date

--- web/frontend/src/components/TripList.jsx
+++ web/frontend/src/components/TripList.jsx
@@ -40,6 +40,15 @@ const TripList = () => {
     });
   };
 
+  // Separate trips into plannedTrips and pastTrips:
+  const currentDate = new Date();
+  const plannedTrips = trips.filter(
+    (trip) => new Date(trip.checkOut) >= currentDate
+  );
+  const pastTrips = trips.filter(
+    (trip) => new Date(trip.checkOut) < currentDate
+  );
+
   return (
     // <div>
     //   <h1 className="text-center">Journey List</h1>
@@ -99,13 +108,49 @@ const TripList = () => {
             <p>Username: susan340958</p>
             {/* <p>Registered since: 01/06/2024</p> */}
             <p>Location: Helsinki, Finland</p>
-            <p>Planned trips: {trips.length}</p>
+            <p>Planned trips: {plannedTrips.length}</p>
           </div>
         </div>
       </div>
-      <h1 className="text-center">Your Saved Plans</h1>
+      <h1 className="text-center">Your Saved Plans:</h1>
       <div className="row justify-content-center">
-        {trips.map((trip) => (
+        {plannedTrips.map((trip) => (
+          <div
+            key={trip.id}
+            className="col-md-10 bg-light p-3 mb-3"
+            style={{ borderRadius: "8px" }}
+          >
+            <div className="d-flex justify-content-between align-items-start">
+              <div>
+                <h2>{trip.destination}</h2>
+                <p>
+                  From {trip.checkIn} to {trip.checkOut}
+                </p>
+              </div>
+              <div
+                className="d-flex flex-column ml-auto"
+                style={{ width: "100px" }}
+              >
+                <Link to={`/edit/${trip.id}`}>
+                  <button className="btn btn-success btn-sm mb-2 w-100">
+                    Edit
+                  </button>
+                </Link>
+                <button
+                  className="btn btn-danger btn-sm w-100"
+                  onClick={() => handleDelete(trip.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <h1 className="text-center">Your Past Journeys:</h1>
+      <div className="row justify-content-center">
+        {pastTrips.map((trip) => (
           <div
             key={trip.id}
             className="col-md-10 bg-light p-3 mb-3"
