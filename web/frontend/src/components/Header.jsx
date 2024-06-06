4284d460b1fd665a195c194ebf498cfb1bd4ad05
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-06-06 13:20:43 +0300

    Updated the homepage, route in the app.jsx

--- web/frontend/src/components/Header.jsx
+++ web/frontend/src/components/Header.jsx
@@ -1,34 +1,35 @@
 import { NavLink } from "react-router-dom";
 import logo from "../assets/images/logo.png";
+import "../assets/css/header.css";
 
 const Header = () => {
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <div className="nav-container">
-          <div className="logo-container">
-            <NavLink className="navbar-brand" to="/">
-              <img src={logo} alt="brand logo" />
-              <h1>
-                Travel Buddy
-                <p>Plan. Explore. Experience the world</p>
-              </h1>
-            </NavLink>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-
+    <header className="header">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid nav-container-local">
+          <NavLink className="navbar-brand" to="/">
+            <img
+              src={logo}
+              width={"100px"}
+              alt="brand logo"
+              className="d-inline-block align-text-center"
+            />{" "}
+            travel buddy
+          </NavLink>
+          <p>Plan. Explore. Experience the world</p>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav  ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink className="nav-link " aria-current="page" to="/">
                   Home
